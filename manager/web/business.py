4bf830ee9038b163d837377274bac452be72d31a
Augusto Roccasalva <augusto@rioplomo.com.ar>
2017-04-17 02:13:17 -0300

    Working on some samples modules

--- manager/web/business.py
+++ manager/web/business.py
@@ -1,10 +1,26 @@
 # -*- coding: utf-8 -*-
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 
 buz = Blueprint('business', __name__, url_prefix='/business')
 
 
 @buz.route('')
 def index():
-    return render_template('business/index.html')
+    return redirect(url_for('.summary'))
+
+@buz.route('/summary')
+def summary():
+    return render_template('business/summary.html')
+
+@buz.route('/accounts')
+def accounts():
+    return render_template('business/accounts.html')
+
+@buz.route('/customers')
+def customers():
+    return render_template('business/customers.html')
+
+@buz.route('/suppliers')
+def suppliers():
+    return render_template('business/suppliers.html')
