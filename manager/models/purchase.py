1d104fb1c486a110c8f9c5e224fef1bce24dd839
Augusto Roccasalva <augusto@rioplomo.com.ar>
2017-04-27 01:04:26 -0300

    Add PurchaseOrder and PurchaseOrderItem models

--- manager/models/purchase.py
+++ manager/models/purchase.py
@@ -56,3 +56,59 @@ class PurchaseDocument(db.Model):
         if self.pos:
             retval = "%04s-%s" % (self.pos, retval)
         return " ".join(self._sort_type.get(self.type), retval)
+
+
+class PurchaseOrder(db.Model):
+    __tablename__ = 'purchase_order'
+
+    STATUS_CANCELLED = 'CANCELLED'
+    STATUS_QUOTING = 'QUOTING'
+    STATUS_PENDING = 'PENDING'
+    STATUS_PARTIAL = 'PARTIAL'
+    STATUS_CONFIRMED = 'CONFIRMED'
+    STATUS_CLOSED = 'CLOSED'
+    STATUS_DRAFT = 'DRAFT'
+
+    STATUSES = [
+        (STATUS_CANCELLED, 'Cancelada'),
+        (STATUS_QUOTING, 'Presupuestando'),
+        (STATUS_PENDING, 'Pendiente'),
+        (STATUS_PARTIAL, 'Parcial'),
+        (STATUS_CONFIRMED, 'Confirmada'),
+        (STATUS_CLOSED, 'Cerrada'),
+        (STATUS_DRAFT, 'Borrador'),
+    ]
+
+    METHOD_EMAIL = 'EMAIL'
+    METHOD_FAX = 'FAX'
+    METHOD_PERSONALLY = 'PERSONALLY'
+
+    METHODS = [
+        (METHOD_MAIL, 'Correo Electrónico'),
+        (METHOD_FAX, 'Fax'),
+        (METHOD_PHONE, 'Telefónico'),
+        (METHOD_PERSONALLY, 'Personalmente'),
+    ]
+
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer)
+    status = db.Column(ChoiceType(STATUSES))
+    notes = db.Column(db.Text)
+    creation_date = db.Column(db.DateTime)
+    method = db.Column(ChoiceType(METHODS))
+
+    supplier_id = db.Column(db.Integer, db.ForeignKey('supplier.id'))
+    supplier = db.relationship(Supplier, backref=db.backref('orders', lazy='dynamic'))
+
+
+class PurchaseOrderItem(db.Model):
+    __tablename__ = 'purchase_order_item'
+
+    id = db.Column(db.Integer, primary_key=True)
+    sku = db.Column(db.String(20))
+    description = db.Column(db.String(60))
+    quantity = db.Column(db.Integer)
+    received_quantity = db.Column(db.Integer, default=0)
+
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
+    order = db.relationship(PurchaseOrder, backref='items')
